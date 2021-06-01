a712e945f08a604af8e5d2495857010c20622ef7
Ziad Gaafar <ziadgaafar@protonmail.com>
2021-06-01 07:50:21 +0200

    added some redirects

--- next.config.js
+++ next.config.js
@@ -2,4 +2,16 @@ module.exports = {
   images: {
     domains: ["image.tmdb.org"],
   },
+  redirects: async () => [
+    {
+      source: "/movies/popular",
+      destination: "/movies/popular/1",
+      permanent: true,
+    },
+    {
+      source: "/series/popular",
+      destination: "/series/popular/1",
+      permanent: true,
+    },
+  ],
 };
